54051b317caecb41d0af634f8a37ed6bc843e0e4
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-01-24 23:00:51 +0900

    third commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,28 +1,34 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 import App from "./App";
 import { Chart } from "./routes/Chart";
 import { Price } from "./routes/Price";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "",
+          element: <Coins />,
+        },
+        {
+          path: ":coinId",
+          element: <Coin />,
+          children: [
+            { path: "chart", element: <Chart /> },
+            { path: "price", element: <Price /> },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Coins />,
-      },
-      {
-        path: ":coinId",
-        element: <Coin />,
-        children: [
-          { path: "chart", element: <Chart /> },
-          { path: "price", element: <Price /> },
-        ],
-      },
-    ],
-  },
-]);
+    basename: "/coinApp",
+  }
+);
+
 export default router;
